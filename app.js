7ff0b6b2196f153fbcb4227d8fbff205864e906d
neekit95 <nikita-ribalkin@mail.ru>
2023-03-04 16:42:52 +0300

    commit

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const buttonZero = document.querySelector('.buttonZero'),
     buttonSix= document.querySelector('.buttonSix'),
     buttonSeven = document.querySelector('.buttonSeven'),
     buttonEight = document.querySelector('.buttonEight'),
-    xo = document.querySelector('.xo');
+    xo = document.querySelector('.xo'),
+    buttonReset = document.querySelector('.reset'),
+    txtXO = document.querySelector('.txtXO');
 
 buttonZero.addEventListener('click', () => enterSymbol(buttonZero) );
 buttonOne.addEventListener('click', () => enterSymbol(buttonOne) );
@@ -18,6 +20,44 @@ buttonFive.addEventListener('click', () => enterSymbol(buttonFive) );
 buttonSix.addEventListener('click', () => enterSymbol(buttonSix) );
 buttonSeven.addEventListener('click', () => enterSymbol(buttonSeven) );
 buttonEight.addEventListener('click', () => enterSymbol(buttonEight) );
+buttonReset.addEventListener('click', () => reset());
+
+function reset () {
+    buttonZero.innerHTML = '';
+    buttonOne.innerHTML = '';
+    buttonTwo.innerHTML = '';
+    buttonThree.innerHTML = '';
+    buttonFour.innerHTML = '';
+    buttonFive.innerHTML = '';
+    buttonSix.innerHTML = '';
+    buttonSeven.innerHTML = '';
+    buttonEight.innerHTML = '';
+    xo.innerHTML = 'X';
+
+    buttonZero.disabled = false;
+    buttonOne.disabled = false;
+    buttonTwo.disabled = false;
+    buttonThree.disabled = false;
+    buttonFour.disabled = false;
+    buttonFive.disabled = false;
+    buttonSix.disabled = false;
+    buttonSeven.disabled = false;
+    buttonEight.disabled = false;
+    
+    buttonZero.style.cursor = 'pointer';
+    buttonOne.style.cursor = 'pointer';
+    buttonTwo.style.cursor = 'pointer';
+    buttonThree.style.cursor = 'pointer';
+    buttonFour.style.cursor = 'pointer';
+    buttonFive.style.cursor = 'pointer';
+    buttonSix.style.cursor = 'pointer';
+    buttonSeven.style.cursor = 'pointer';
+    buttonEight.style.cursor = 'pointer';
+    txtXO.innerHTML = `Turn <span> ${xo.innerHTML} </span> `;
+
+
+}
+
 
 function checkXO () {
     if (xo.innerHTML == 'X') {
@@ -28,12 +68,48 @@ function checkXO () {
 }
 
 function enterSymbol (button) {
-
+    
     button.innerHTML = xo.innerHTML;
     button.style.cursor = 'auto';
-    button.classList = 'filled';
     button.disabled = true;
     checkXO();
-
-    button.addEventListener('mouseout', () => {button.style.color = 'red'});
+    isEnd ()
 }
+
+function isEnd () {
+    if (buttonZero.innerHTML !== '' 
+        && buttonOne.innerHTML !== '' 
+        && buttonTwo.innerHTML !== ''
+        && buttonThree.innerHTML !== '' 
+        && buttonFour.innerHTML !== '' 
+        && buttonFive.innerHTML !== '' 
+        && buttonSix.innerHTML !== '' 
+        && buttonSeven.innerHTML !== '' 
+        && buttonEight.innerHTML !== ''
+        ) {
+            txtXO.innerHTML = 'Game is <span>over</span>'
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // buttonZero
+        // buttonOne
+        // buttonTwo
+        // buttonThree
+        // buttonFour
+        // buttonFive
+        // buttonSix
+        // buttonSeven
+        // buttonEight
